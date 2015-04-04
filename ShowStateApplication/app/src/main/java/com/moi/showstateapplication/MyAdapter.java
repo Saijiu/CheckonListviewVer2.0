e30d52f596e9f7bba619f9023a637e114d236e9e
moiling <super8moi@gmail.com>
2015-04-04 16:24:40 +0800

    holder

--- ShowStateApplication/app/src/main/java/com/moi/showstateapplication/MyAdapter.java
+++ ShowStateApplication/app/src/main/java/com/moi/showstateapplication/MyAdapter.java
@@ -1,7 +1,9 @@
 package com.moi.showstateapplication;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,20 +28,46 @@ public class MyAdapter extends ArrayAdapter<Item> {
         resouceId = resource;
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Item item = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resouceId, null);
-        ImageView imageView = (ImageView) view.findViewById(R.id.iv_item);
-        TextView textView = (TextView)view.findViewById(R.id.tv_item);
-        imageView.setImageResource(item.getImageId());
-        textView.setText(item.getText());
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resouceId, null);
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = (ImageView) view.findViewById(R.id.iv_item);
+            viewHolder.textView = (TextView) view.findViewById(R.id.tv_item);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
         if (position == Postion.getInstance().getPostion()) {
             view.setBackgroundColor(view.getResources().getColor(R.color.gray));
-            textView.setTextColor(view.getResources().getColor(R.color.white));
-            textView.setText(textView.getText().toString() + "              Choose uncheck");
-            imageView.setColorFilter(0xFFFFFFFF, PorterDuff.Mode.SRC_IN);
+            viewHolder.textView.setTextColor(view.getResources().getColor(R.color.white));
+            viewHolder.textView.setText(viewHolder.textView.getText().toString() + "              Choose uncheck");
+            viewHolder.imageView.setImageResource(item.getImageId());
+            viewHolder.imageView.setColorFilter(0xFFFFFFFF, PorterDuff.Mode.SRC_IN);
+        } else {
+            if (convertView != null) {
+                convertView.setBackground(null);
+                viewHolder = (ViewHolder) convertView.getTag();
+                viewHolder.textView.setText(item.getText());
+                viewHolder.textView.setTextColor(view.getResources().getColor(R.color.gray));
+                viewHolder.imageView.clearColorFilter();
+            } else {
+                viewHolder.imageView.setImageResource(item.getImageId());
+                viewHolder.textView.setText(item.getText());
+            }
         }
         return view;
+    }
+
+    class ViewHolder {
+        ImageView imageView;
+        TextView textView;
     }
 }
